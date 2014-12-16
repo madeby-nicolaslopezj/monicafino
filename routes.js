612314bdc1e25ee47950c49619e35bab6fe2c39b
Nicolás López <nicolaslopezj@me.com>
2014-12-16 18:32:50 -0300

    added seo

--- routes.js
+++ routes.js
@@ -13,6 +13,25 @@ Router.map(function() {
 		path: '/',
 		loadingTemplate: 'adminLoading',
 		waitOn: subscriptions,
+		onAfterAction: function() {
+			if (!Meteor.isClient) {
+				return;
+			}
+			SEO.set({
+				title: orion.dictionary.get('seoTitle'),
+				link: {
+					icon: orion.dictionary.get('seoFavIcon.link'),
+				},
+				meta: {
+					'description': orion.dictionary.get('seoDescription')
+				},
+				og: {
+					'title': orion.dictionary.get('seoTitle'),
+					'description': orion.dictionary.get('seoDescription'),
+					'image': orion.dictionary.get('seoImage.link')
+				}
+			});
+		}
 	});
 
 	this.route('post', {
@@ -21,6 +40,26 @@ Router.map(function() {
 		waitOn: subscriptions,
 		data: function() {
 			return orion.entities.news.collection.findOne({_id: this.params._id});
+		},
+		onAfterAction: function() {
+			if (!Meteor.isClient) {
+				return;
+			}
+			var post = orion.entities.news.collection.findOne({_id: this.params._id});
+			SEO.set({
+				title: post.title,
+				link: {
+					icon: orion.dictionary.get('seoFavIcon.link'),
+				},
+				meta: {
+					'description': post.preview
+				},
+				og: {
+					'title': post.title,
+					'description': post.preview,
+					'image': post.image.link
+				}
+			});
 		}
 	});
 
